a38ca0ca4818934f4dc09c57bf656310e4be5c75
camroddur9 <ui.crodriguezd@gmail.com>
2022-08-30 18:41:02 -0500

    Express routing

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Server in express')
 });
 
+app.get('/new-route', (req,res) => {
+  res.send('New endpoint')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: "Product 1",
+    price: 1000
+  })
+})
+
 app.listen(port, () => {
   console.log('running on port ' + port)
 });
