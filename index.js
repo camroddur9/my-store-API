0623ca36b75959df1fbadddee9c734d21a0c0adc
camroddur9 <ui.crodriguezd@gmail.com>
2022-08-30 18:37:13 -0500

    server running on express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("My App")
+const express = require('express')
+const app = express()
+
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Server in express')
+});
+
+app.listen(port, () => {
+  console.log('running on port ' + port)
+});
