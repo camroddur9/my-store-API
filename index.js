0badd1bee18e65bb8924ade4db01416b0a0939f5
camroddur9 <ui.crodriguezd@gmail.com>
2022-08-30 19:09:40 -0500

    getting params

--- index.js
+++ index.js
@@ -7,15 +7,42 @@ app.get('/', (req, res) => {
   res.send('Server in express')
 });
 
-app.get('/new-route', (req,res) => {
-  res.send('New endpoint')
+app.get('/products', (req, res) => {
+  res.json(
+    [
+      {
+        name: "Product 1",
+        price: 1000
+      },
+      {
+        name: "Product 2",
+        price: 2000
+      }
+    ]
+  )
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id }= req.params
+  res.json(
+    {
+      id,
+      name: "products 2",
+      price: 2000
+    }
+  )
 })
 
-app.get('/products', (req, res) => {
-  res.json({
-    name: "Product 1",
-    price: 1000
-  })
+app.get('/categories/:categoryId/products/:productsId', (req,res) => {
+  const {categoryId, productsId} = req.params
+  res.json(
+    {
+      categoryId,
+      productsId,
+      name: "products 2",
+      price: 2000
+    }
+  )
 })
 
 app.listen(port, () => {
