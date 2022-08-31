a5a75d4b063369b9b5c60e2d4b405ada3d1a2cdd
camroddur9 <ui.crodriguezd@gmail.com>
2022-08-30 19:26:56 -0500

    query params

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const faker = require('faker')
 const app = express()
 
 const port = 8080
@@ -8,19 +9,24 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json(
-    [
-      {
-        name: "Product 1",
-        price: 1000
-      },
-      {
-        name: "Product 2",
-        price: 2000
-      }
-    ]
-  )
+  const { size } = req.query
+  const limit = size || 10
+  const products = []
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.imageUrl()
+    })
+  }
+
+  res.json(products)
 });
+
+app.get('/products/filter', (req, res) => {
+  res.send('filter')
+})
 
 app.get('/products/:id', (req, res) => {
   const { id }= req.params
@@ -43,6 +49,19 @@ app.get('/categories/:categoryId/products/:productsId', (req,res) => {
       price: 2000
     }
   )
+})
+
+app.get('/user', (req,res) => {
+  const { limit, offset } = req.query
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  }
+  else{
+    res.send('no query params')
+  }
 })
 
 app.listen(port, () => {
